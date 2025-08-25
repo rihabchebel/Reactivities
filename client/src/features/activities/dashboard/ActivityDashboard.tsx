16ad4946b7646b2bb68cfa8a038e0e18b13edb2b
Rihab chebel <chebelrihab7@gmail.com>
2025-08-25 14:55:02 +0100

    End of section 7

--- client/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,43 +1,16 @@
 import { Grid2 } from "@mui/material";
 import ActivityList from "./ActivityList";
-import ActivityDetail from "../details/ActivityDetail";
-import ActivityForm from "../form/ActivityForm";
-type Props = {
-    activities: Activity[]
-    selectActivity: (id: string) => void;
-    CancelSelectActivity: () => void;
-    selectedActivity?: Activity;
-    openForm: (id: string) => void;
-    closeForm: () => void;
-    editMode: boolean;
-  
 
 
-}
-
-export default function ActivityDashboard({
-  activities, selectActivity, CancelSelectActivity,
-   selectedActivity, openForm, closeForm, editMode,
-   }: Props) {
+export default function ActivityDashboard() {
+   
   return (
     <Grid2 container spacing={3}>
         <Grid2 size={7}>
-          <ActivityList
-            activities={activities}
-            selectActivity={selectActivity}
-          />
+          <ActivityList />
         </Grid2>
         <Grid2 size={5}>
-          {selectedActivity && !editMode &&
-          <ActivityDetail 
-              selectedActivity={selectedActivity} 
-              cancelSelectActivity={CancelSelectActivity}
-              openForm={openForm}
-          />}
-          {editMode &&
-          <ActivityForm
-           closeForm={closeForm} activity={selectedActivity}
-           />}
+          activity filters go here 
         </Grid2>
 
     </Grid2>
--- client/src/features/activities/form/ActivityForm.tsx
+++ client/src/features/activities/form/ActivityForm.tsx
@@ -1,66 +1,85 @@
 import { Box, Paper, TextField, Typography, Button } from "@mui/material";
 import type { FormEvent } from "react";
 import { useActivities } from "../../../lib/hooks/useActivities";
+import { useNavigate, useParams } from "react-router";
 
-type Props = {
-    activity?: Activity;
-    closeForm: () => void;
-   
-    
-}
+export default function ActivityForm() {
+  const {id}= useParams();
+  const { updateActivity, createActivity, activity, isLoadingActivity } = useActivities(id);
+  const navigate = useNavigate();
 
-export default function ActivityForm({activity, closeForm} :Props) {
-    const {updateActivity, createActivity} = useActivities();
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    const handleSubmit =async (event: FormEvent<HTMLFormElement>) =>{
-        event.preventDefault();
-        
-        const formData = new FormData(event.currentTarget);
-        const data :{[key: string]: FormDataEntryValue} ={};
-        formData.forEach((value, key) => {
-            data[key] = value;
-        });
+    const formData = new FormData(event.currentTarget);
+    const data: { [key: string]: FormDataEntryValue } = {};
+    formData.forEach((value, key) => {
+      data[key] = value;
+    });
 
-        if (activity) {
-            data.id = activity.id;
-            await updateActivity.mutateAsync(data as unknown as Activity);
-            closeForm();
+    if (activity) {
+      data.id = activity.id;
+      await updateActivity.mutateAsync(data as unknown as Activity);
+      navigate(`/activities/${activity.id}`);
+    } else {
+      await createActivity.mutate(data as unknown as Activity, {
+        onSuccess: (id) => {
+            navigate(`/activities/${id}`)
         }
-        else{
-            await createActivity.mutateAsync(data as unknown as Activity);
-            closeForm();
-        }
-
-        
+      });
     }
+  }
+    if(isLoadingActivity) return <Typography>Loading Activity...</Typography>
 
   return (
-    <Paper sx={{borderRadius: 3, padding: 3 }}>
-        <Typography variant="h5" gutterBottom color="primary">
-            Create Activity
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} display="flex" flexDirection="column" gap={3}>
-            <TextField name="title" label="Title" defaultValue={activity?.title} />
-            <TextField name="description"  label="Description" defaultValue={activity?.description} multiline rows={3} />
-            <TextField name="category"  label="Category" defaultValue={activity?.category} />
-            <TextField name="date" label="Date"
-               defaultValue={activity?.date
-                ? new Date(activity.date).toISOString().split('T')[0]
-                : new Date().toISOString().split('T')[0]
-               } type="date" />
-            <TextField name="city"  label="City" defaultValue={activity?.city} />
-            <TextField name="venue"  label="Venue" defaultValue={activity?.venue} />
-            <Box display="flex" justifyContent="flex-end" gap={2}>
-                <Button onClick={closeForm} color="inherit">Cancel</Button>
-                <Button type="submit" color="success"
-                 variant="contained"
-                  disabled={updateActivity.isPending || createActivity.isPending}
-                >Submit</Button>
-            </Box>
-
+    <Paper sx={{ borderRadius: 3, padding: 3 }}>
+      <Typography variant="h5" gutterBottom color="primary">
+        {activity ? "Edit Activity" : "Create Activity"}
+      </Typography>
+      <Box
+        component="form"
+        onSubmit={handleSubmit}
+        display="flex"
+        flexDirection="column"
+        gap={3}
+      >
+        <TextField name="title" label="Title" defaultValue={activity?.title} />
+        <TextField
+          name="description"
+          label="Description"
+          defaultValue={activity?.description}
+          multiline
+          rows={3}
+        />
+        <TextField
+          name="category"
+          label="Category"
+          defaultValue={activity?.category}
+        />
+        <TextField
+          name="date"
+          label="Date"
+          defaultValue={
+            activity?.date
+              ? new Date(activity.date).toISOString().split("T")[0]
+              : new Date().toISOString().split("T")[0]
+          }
+          type="date"
+        />
+        <TextField name="city" label="City" defaultValue={activity?.city} />
+        <TextField name="venue" label="Venue" defaultValue={activity?.venue} />
+        <Box display="flex" justifyContent="flex-end" gap={2}>
+          <Button color="inherit">Cancel</Button>
+          <Button
+            type="submit"
+            color="success"
+            variant="contained"
+            disabled={updateActivity.isPending || createActivity.isPending}
+          >
+            Submit
+          </Button>
         </Box>
-
+      </Box>
     </Paper>
-    
-  )
+  );
 }
